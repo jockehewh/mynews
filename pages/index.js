d4bdd0aafe54a22410373beaeb90f8dba356f097
Ayoub Bhd <ayoubrelizen@hotmail.fr>
2023-11-14 17:08:04 +0100

    creation de la navbar

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import styles from '@/styles/Home.module.css';
+import Navbar from '@/components/Navbar';
+import styles from '@/styles/Navbar.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { increment, decrement, addName } from '@/reducers/example';
-import { useState } from 'react';
+
 export default function Home() {
-  const dispatch = useDispatch()
-  const counter = useSelector(state => state.counter.value)
-  const [name, setName] = useState("")
   return (
-    <>
-      
-    </>
+    <Navbar />
   )
 }
