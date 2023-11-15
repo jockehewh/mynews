468c8eeae2dbca71c6b52d9a4f305d968d18e4f2
xavier.belenus <xavier.belenus@gmail.com>
2023-11-15 11:49:56 +0100

    fix slide.js

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import style from '@/styles/Slider.module.css'
 import Slide from "./Slide"
+import { useSelector } from 'react-redux';
 export default function Slider() {
+  const trending = useSelector(state => state.news.value.trendingArticles)
   return (
     <div className = {style.trending} >
       <h1>Trending</h1>
       <div className={style.slider}>
         <div>left</div>
-        <div className={style.slides}><Slide/></div>
+        <div className={style.slides}>{trending.length > 0 ? <Slide/> : ""}</div>
         <div>right</div>
       </div>
     </div>
