f172ca2f39d2859a909c7d1fb8e0101bcc9656d9
xavier.belenus <xavier.belenus@gmail.com>
2023-11-15 16:26:34 +0100

    update

--- components/Slide.jsx
+++ components/Slide.jsx
@@ -6,7 +6,7 @@ export default function Slide(props) {
   return (
     <a className={style.slide} href={props.article.url} target="_blank">
       <img src={props.article.urlToImage} alt={props.article.title} />
-      <div className={style.details}>
+      <div className={style.details}> 
         <p>Published the <span>{new Intl.DateTimeFormat("us-US", {dateStyle: "medium"}).format(new Date(props.article.publishedAt))}</span></p>
         <h1>{props.article.title}</h1>
         <p>{props.article.content.split("[+")[0]}</p>
--- reducers/news.js
+++ reducers/news.js
@@ -1,7 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: { mainArticles: [], trendingArticles: [] }
+  value: { mainArticles: [], trendingArticles: [], favorites: [
+    {
+      "source": {
+        "id": "wired",
+        "name": "Wired"
+      },
+      "author": "Tristan Kennedy",
+      "title": "Skiing Is Getting Riskier",
+      "description": "The threat of avalanches is rising with global warming, but technology can help protect skiers on and off the slopes.",
+      "url": "https://www.wired.com/story/avalanche-threat-ski-risk/",
+      "urlToImage": "https://media.wired.com/photos/654ece1ce8bd90ff3e462809/191:100/w_1280,c_limit/offpiste-sci-GettyImages-1749819283-(1).jpg",
+      "publishedAt": "2023-11-11T12:00:00Z",
+      "content": "As Olivier Gardet piloted the drone around the mountain, his colleague, who was looking through goggles connected to its infrared camera, could see the avalanche clearly: a long tongue of debris, vis… [+4171 chars]"
+    },
+    {
+      "source": {
+        "id": "wired",
+        "name": "Wired"
+      },
+      "author": "Lily Hay Newman",
+      "title": "This Cryptomining Tool Is Stealing Secrets",
+      "description": "Plus: Details emerge of a US government social media-scanning tool that flags “derogatory” speech, and researchers find vulnerabilities in the global mobile communications network.",
+      "url": "https://www.wired.com/story/cryptominer-espionage-campaign-security-roundup/",
+      "urlToImage": "https://media.wired.com/photos/653c26a5a4dd0111031f340f/191:100/w_1280,c_limit/Security-News-This-Week--A-Dull-Cryptominer-Is-Actually-Part-of-a-Sophisticated-and-Expansive-Espionage-Campaign-Security-GettyImages-1087732912.jpg",
+      "publishedAt": "2023-10-28T13:00:00Z",
+      "content": "As the Israel-Hamas war raged on this week and Israel expanded its ground invasion of the Gaza Strip, the territory's compromised internet infrastructure and access to connectivity went fully dark on… [+5479 chars]"
+    },
+  ] }
 }
 
 export const newsSlice = createSlice({
@@ -16,8 +43,12 @@ export const newsSlice = createSlice({
       console.log("main", action.payload)
       state.value.mainArticles = action.payload
     },
+    setFavorites: (state, action) => {
+      console.log("main", action.payload)
+      state.value.favorites.push(action.payload)
+    },
   }
 })
 
-export const { setTrendingArticles, setMainArticles } = newsSlice.actions
+export const { setTrendingArticles, setMainArticles, setFavorites } = newsSlice.actions
 export default newsSlice.reducer
