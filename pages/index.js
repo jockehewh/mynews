62f6301f78b7f3a5db2f29d8ad97c24e9764b3ff
Shino75 <Safre.adam.pro@gmail.com>
2023-11-16 12:26:27 +0100

    Update Article and CSS

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,38 @@
-import Slider from '@/components/Slider';
-import { useDispatch, useSelector } from 'react-redux';
-import { setTrendingArticles, setMainArticles } from '@/reducers/news';
-import { useEffect, useState } from 'react';
+import Slider from "@/components/Slider";
+import { useDispatch, useSelector } from "react-redux";
+import { setTrendingArticles, setMainArticles } from "@/reducers/news";
+import { useEffect, useState } from "react";
+import Article from "@/components/Article";
+import Navbar from "@/components/Navbar";
+import Slide from "@/components/Slide";
+import ContainerArticle from "@/components/ContainerArticle";
+
 export default function Home() {
-  const [trendings, setTrendings] = useState([])
-  const dispatch = useDispatch()
-  const obj = useSelector(state => state.news.value.trendingArticles)
-  useEffect(()=>{
-    fetch("https://newsapi.org/v2/everything?q=tech&apiKey=57e20f764388443d9513de7fb3d03539")
-      .then(res => res.json())
-      .then(data => data.articles)
-      .then(articles => {
-        let trending = []
-        trending.push(articles.shift())
-        trending.push(articles.shift())
-        trending.push(articles.shift())
-        trending.push(articles.shift())
-        dispatch(setTrendingArticles(trending))
-        dispatch(setMainArticles(articles))
-        setTrendings([...trending])
-      })
-  }, [])
+  const [trendings, setTrendings] = useState([]);
+  const dispatch = useDispatch();
+  const obj = useSelector((state) => state.news.value.trendingArticles);
+  useEffect(() => {
+    fetch(
+      "https://newsapi.org/v2/everything?q=tech&apiKey=57e20f764388443d9513de7fb3d03539"
+    )
+      .then((res) => res.json())
+      .then((data) => data.articles)
+      .then((articles) => {
+        let trending = [];
+        trending.push(articles.shift());
+        trending.push(articles.shift());
+        trending.push(articles.shift());
+        trending.push(articles.shift());
+        dispatch(setTrendingArticles(trending));
+        dispatch(setMainArticles(articles));
+        setTrendings([...trending]);
+      });
+  }, []);
   return (
     <>
+      <Navbar />
+      <Slider />
+      <ContainerArticle />
     </>
-  )
+  );
 }
